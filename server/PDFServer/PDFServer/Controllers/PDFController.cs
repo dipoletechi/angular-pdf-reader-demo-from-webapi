36c3982c88d89a9a2770121ffe4f641845efcf3f
Dhamrandar Kumar <dipoletechi@gmail.com>
2019-12-20 19:52:38 +0530

    added pending code

---
+++ server/PDFServer/PDFServer/Controllers/PDFController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PDFServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PDFController : ControllerBase
+    {
+        [HttpGet]
+        public object Get()
+        {
+            return "Welcome to the test";
+        }
+        // GET api/values
+        [HttpGet]
+        [Route("getpdf")]
+        public object GetPDF()
+        {
+            var pdfdata = System.IO.File.ReadAllBytes("sample.pdf");
+            var bytesdata= Convert.ToBase64String(pdfdata, 0, pdfdata.Length);
+          
+            return bytesdata;
+        }     
+    }
+}
